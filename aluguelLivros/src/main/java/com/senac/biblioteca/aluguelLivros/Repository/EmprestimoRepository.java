968ad41d1b70d8b5193fa28c519b75ee017b23db
Alex Costa <149431146+Alexco669@users.noreply.github.com>
2024-10-13 01:20:34 -0300

    Delete aluguelLivros directory

--- aluguelLivros/src/main/java/com/senac/biblioteca/aluguelLivros/Repository/EmprestimoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.senac.biblioteca.aluguelLivros.Repository;
-
-import com.senac.biblioteca.aluguelLivros.Models.Emprestimo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmprestimoRepository extends JpaRepository<Emprestimo, Integer> {
-}
